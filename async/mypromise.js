8cf5e1264ac6a8c17093104107bcbd3d3f5279f8
hongyang <waiting2511@gmail.com>
2019-08-11 19:40:43 +0800

    promise支持异步操作

--- async/mypromise.js
+++ async/mypromise.js
@@ -1,5 +1,4 @@
 // 实现promise
-
 class MyPromise {
     constructor(executor) {
         // 初始化promise状态
@@ -7,12 +6,18 @@ class MyPromise {
         // 初始化 value 和 reason 分别用于promise resolve和 reject时使用
         this.value = undefined;
         this.reason = undefined;
+        // 成功存放的数组
+        this.onResolvedCallbacks = [];
+        this.onRejectedCallbacks = [];
         // 定义promise中的resolve方法
         const resolve = (value) => {
             // 只有在 pending状态时，promise才是可变的
             if (this.status === 'pending') {
                 this.status = 'fulfilled';
                 this.value = value;
+
+                // 一旦resolve执行，调用成功数组的函数
+                this.onResolvedCallbacks.forEach(fn=>fn());
             }
         }
         // 定义promise中的reject方法
@@ -20,6 +25,9 @@ class MyPromise {
             if (this.status === 'pending') {
                 this.status = 'rejected';
                 this.reason = reason;
+
+                // 一旦reject执行，调用失败数组的函数
+                this.onRejectedCallbacks.forEach(fn=>fn());
             }
         }
 
